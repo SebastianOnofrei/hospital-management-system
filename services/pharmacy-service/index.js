b78b99ab3fd84fa68cc39a549beb7089d54b4f66
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-08-07 23:54:32 +0300

    feature/HMS-003-create-script-that-start-each-server-concurrently created script that starts each service concurrentyl from our root

--- services/pharmacy-service/index.js
+++ services/pharmacy-service/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3007;
 
 app.get("/", (req, res) => {
-  res.send("Hellooooo from service");
+  res.send("Hellooooo from Pharmacy service");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on localhost port ${PORT}`);
+  console.log(`Pharmacy service started on localhost port ${PORT}`);
 });
