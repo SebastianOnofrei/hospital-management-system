edd7e3fe03913e7eaee2fbc15ddaf091f290ee1e
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-08-11 23:49:54 +0300

    feature/HMS-010-add-basic-security-packages added basic security packages - helmet, cors, rate limiting to the api gateway

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 const { checkService } = require("../../utils/helpers/checkServiceHealth");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.disable("x-powered-by");
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+app.use(
+  express.json({
+    limit: "1mb",
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
+
+app.use(limiter);
 
 app.use(
   "/api/appointment",
