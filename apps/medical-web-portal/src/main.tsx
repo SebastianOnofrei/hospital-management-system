fca7bd09d7fe13faf521afd92714f8d309689e31
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-08-13 00:31:00 +0300

    feature/HMS-009-add-i18n-to-frontend-projects made setup for i18n for the frontend apps

--- apps/medical-web-portal/src/main.tsx
+++ apps/medical-web-portal/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import "./i18n/config";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
