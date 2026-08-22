42cdb82fa08bce8d24c44980b622789020b8a030
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-08-23 00:36:05 +0300

    feature/HMS-007-create-logging-architecture added request id for each HTTP request

--- packages/logger/src/create-http-logger.js
+++ packages/logger/src/create-http-logger.js
@@ -1,5 +1,24 @@
 import pinoHttp from "pino-http";
+import crypto from "node:crypto";
 
 export function createHttpLogger(logger) {
-  return pinoHttp({ logger });
+  return pinoHttp({
+    logger,
+
+    genReqId: (req, res) => {
+      const incomingRequestId = req.headers["x-request-id"];
+
+      if (incomingRequestId) {
+        res.setHeader("x-request-id", incomingRequestId);
+
+        return incomingRequestId;
+      }
+
+      const requestId = crypto.randomUUID();
+
+      res.setHeader("x-request-id", requestId);
+
+      return requestId;
+    },
+  });
 }
