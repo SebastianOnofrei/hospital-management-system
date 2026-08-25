0d3ba0b1af9eb4cb4275a9524fda252bb63cb16a
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2026-08-25 14:21:36 +0300

    feature/HMS-081-create-button-atom-element (#30)
    * feature/HMS-081-create-button-atom-element created button elements for both frontends
    
    * feature/HMS-081-create-button-atom-element deleted trim method

--- apps/admin-dashboard/src/components/atoms/Button.tsx
+++ apps/admin-dashboard/src/components/atoms/Button.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+// Defining prop types for the Button component
+interface ButtonProps {
+  text: string;
+  className?: string;
+  disabled?: boolean;
+  onclick?: () => void;
+}
 
-const Button = () => {
-  return <div>Button</div>;
+// Exporting the Button component as a functional component with defined prop types
+export const Button: React.FC<ButtonProps> = ({ text, className = '', disabled = false, onclick, }) => {
+  return (
+    <button
+      type="button"
+      className={`btn ${className}`.trim()}
+      disabled={disabled}
+      onClick={onclick}
+    >
+      {text}
+    </button>
+  );
 };
-
-export default Button;
---
+++ apps/medical-web-portal/src/components/atoms/Button.tsx
@@ -0,0 +1,21 @@
+// Defining prop types for the Button component
+interface ButtonProps {
+  text: string;
+  className?: string;
+  disabled?: boolean;
+  onclick?: () => void;
+}
+
+// Exporting the Button component as a functional component with defined prop types
+export const Button: React.FC<ButtonProps> = ({ text, className = '', disabled = false, onclick, }) => {
+  return (
+    <button
+      type="button"
+      className={`btn ${className}`}
+      disabled={disabled}
+      onClick={onclick}
+    >
+      {text}
+    </button>
+  );
+};
