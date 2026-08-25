676c3a40d69152714e1c230fbb2d3e3b6bd963a3
franc51 <francisc_szasz@yahoo.com>
2026-08-25 16:38:25 +0300

    feature/HMS-096-create-generic-header-component modified header component to take props

--- apps/medical-web-portal/src/components/pages/Home.tsx
+++ apps/medical-web-portal/src/components/pages/Home.tsx
@@ -1,11 +1,30 @@
-import Header from "../organisms/Header/Header";
-import Footer from "../organisms/Footer/Footer";
+import {Header} from "../organisms/Header/Header.tsx";
+import Footer from "../organisms/Footer/Footer.tsx";
 import './Home.css';
+import logo from '../../assets/pictures/gg-logo.jpg';
+import { Button } from "../atoms/Button.tsx";
+
+const headerLinks = [
+  { label: 'About', href: '/about' },
+  { label: 'Doctors', href: '/doctors' },
+  { label: 'Prices', href: '/prices' },
+  { label: 'Performances', href: '/performances' },
+  { label: 'Contributions', href: '/contributions' },
+];
 
 const Home = () => {
  return (
     <div className="home-layout">
-      <Header />
+      <Header 
+         logo={logo}
+         links={headerLinks} 
+         action={
+          <div className="header-buttons">
+            <Button text="Appointments" className="btn"/>
+            <Button text="Login" className="btn-primary" />
+          </div>
+        }
+      />
       <main className="main-content">
         {/* Page content or loading state goes here */}
       </main>
