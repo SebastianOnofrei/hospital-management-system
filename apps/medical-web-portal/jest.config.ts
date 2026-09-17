ef9115bf88d708b162a05f9aa22c5dceaf28469a
franc51 <francisc_szasz@yahoo.com>
2026-09-17 09:59:32 +0300

    feature/HMS-083-jest-final fixed structure

--- apps/medical-web-portal/jest.config.ts
+++ apps/medical-web-portal/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setupTests.ts'],
   transform: {
     '^.+\\.(t|j)sx?$': [
       '@swc/jest',
